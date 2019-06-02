7490e0702d388d5e6374c84c3b0c937580de6da8
Michael Robertson <mjr9804@gmail.com>
2019-06-02 19:35:17 -0400

    Add baseUrl env variable

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ApiService {
-    private api = 'http://localhost:3000';
+    private api = environment.baseUrl;
     private listUrl = `${this.api}/quiz`;
     private detailsUrl = `${this.api}/details`;
     private takeUrl = `${this.api}/take`;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'http://ec2-54-145-56-218.compute-1.amazonaws.com:3000',
 };
